17de4323925ca8304ad79e78cfe1ac63b49b7007
Abhishek Atnure <atnureabhishek@gmail.com>
2020-12-13 14:32:12 +0530

    style changed some styles

--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -11,26 +11,23 @@ function Posts({ isLoading, items }) {
         return timeago.format(time * 1000)
     }
 
-    //jsx to render
+    // //jsx to render
     return isLoading ? <Loading /> : (
         <div className="blocks">
             {
                 items.children.length > 0 && items.children.map(post =>
                     <div className="fields">
-                        <h4>{post.data.title}</h4>
-                        <TextTruncate
-                            line={1}
-                            element="span"
-                            truncateText="…"
-                            text={post.data.selftext}
-                            textTruncateChild={<a href={post.data.url} target="_blank" >View</a>}
-                        />
+                        <h6>{post.data.title}{post.data.selftext ? <a href={post.data.url} target="_blank">View Post</a> : null}</h6>
+
                         {post.data.post_hint === 'image' ?
                             <img src={post.data.url_overridden_by_dest} alt={post.data.description} /> : null}
                         <div className="bottom-span">
-                            <span>created  {createdOn(post.data.created_utc)}</span>
+                            <span>{createdOn(post.data.created_utc)}</span>
+                            <span>by {post.data.author}</span>
+
                         </div>
                     </div>
+
                 )}
         </div>
     )
